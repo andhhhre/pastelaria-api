62e3196f3383f4ab9d50c70e5a3acb9d7497c846
André Costa <andrec@hapvida.com.br>
2019-12-01 21:18:11 -0300

    2.5. Criando um projeto Spring Boot com Spring Initializr

---
+++ src/main/java/com/andrecosta/pastelaria/PastelariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.andrecosta.pastelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelariaApiApplication.class, args);
+	}
+
+}
